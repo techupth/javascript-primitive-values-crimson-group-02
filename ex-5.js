e1db4d77bcf63c7e244eea0c40412c9c37250410
Tanawut Wongboot <tanawut.w@outlook.com>
2023-11-23 14:04:01 +0700

    feat(ex-5): completed ex-5

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let percentDiscount = 25;
+let sumDiscountTHB = totalPrice * (percentDiscount / 100);
+let totalNetPrice = totalPrice - sumDiscountTHB;
