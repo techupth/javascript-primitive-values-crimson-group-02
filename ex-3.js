b8f8f05032f4d46b675e94c35d6ee672ee291611
Tanawut Wongboot <tanawut.w@outlook.com>
2023-11-23 13:34:39 +0700

    feat(ex-3): completed ex-3

--- ex-3.js
+++ ex-3.js
@@ -4,3 +4,5 @@ let totalFileSize = 250000;
 let uploadedSize = 30000;
 
 // Start coding here
+let uploadProgress = (uploadedSize / totalFileSize) * 100;
+console.log("🚀 ~ file: ex-3.js:9 ~ uploadProgress:", uploadProgress + "%")
