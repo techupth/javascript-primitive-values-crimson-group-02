a58d765dd18f3b6b2360389a30ff446c4b740102
Tanawut Wongboot <tanawut.w@outlook.com>
2023-11-23 13:38:07 +0700

    feat(ex-4): completed ex-4

--- ex-4.js
+++ ex-4.js
@@ -5,3 +5,4 @@ let numberOfSoldProduct = 12000;
 let numberOfProductInStock = undefined;
 
 console.log((numberOfSoldProduct / numberOfProductInStock) * 100);
+// จะแสดงผลเป็น NaN เนื่องจาก ไม่สามารถนำ Number ไปคำนวณผลทางคณิตศาสตร์ร่วมกับ String ได้
